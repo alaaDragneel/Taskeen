36a1f184704e960baf72464a3b8e208116e37c1b
alaaDragneel <githup/alaaDragneel>
2017-03-15 03:17:47 +0200

    New DB

--- taskeen_store.sql
+++ taskeen_store.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2017 at 07:15 AM
+-- Generation Time: Mar 15, 2017 at 02:16 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -57,6 +57,7 @@ CREATE TABLE `buldings` (
   `address` varchar(255) NOT NULL,
   `price` varchar(255) NOT NULL,
   `num_pr` varchar(255) NOT NULL,
+  `num_kit` varchar(255) NOT NULL,
   `num_rooms` varchar(255) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0',
   `type` int(11) NOT NULL DEFAULT '0',
@@ -65,8 +66,40 @@ CREATE TABLE `buldings` (
   `subarea_id` int(10) NOT NULL,
   `user_id` int(10) NOT NULL,
   `categoury_id` int(10) NOT NULL,
-  `subcategoury_id` int(10) NOT NULL
+  `subcategoury_id` int(10) NOT NULL,
+  `image` varchar(300) NOT NULL,
+  `month` varchar(3) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `isApproved` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `buldings`
+--
+
+INSERT INTO `buldings` (`id`, `title`, `description`, `address`, `price`, `num_pr`, `num_kit`, `num_rooms`, `status`, `type`, `city_id`, `area_id`, `subarea_id`, `user_id`, `categoury_id`, `subcategoury_id`, `image`, `month`, `year`, `isApproved`, `created_at`, `updated_at`) VALUES
+(2, 'test 2`', 'test description 2', 'test address 2', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-07-44-34_f50112e1a66318535df4d36e9c7521a5de42e340.jpg', '03', '2017', 1, '2017-03-14 18:44:34', '2017-03-14 19:21:03'),
+(3, 'test3', 'test desciption3', 'test address3', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-59-07_0d76c634444a843f3c1ef7dee1663b2353541da2.jpg', '04', '2017', 1, '2017-03-14 18:02:55', '2017-03-14 22:59:07'),
+(5, 'test5', 'test desciption5', 'test address5', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-58-32_10fad5e66d64afe67e8e6a700e6385b6e7640c67.jpg', '06', '2017', 1, '2017-03-14 18:02:55', '2017-03-14 22:58:32'),
+(6, 'test 6', 'test description 6', 'test address 6', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-57-45_ddaf6890dc9a1054168c10ae4a8f9fe2ee154d79.jpg', '06', '2017', 0, '2017-03-14 18:44:34', '2017-03-14 22:57:45'),
+(7, 'test3', 'test desciption3', 'test address3', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-57-27_0bba3e5fc28ab4a35224462517daf1f2902cdb2f.jpg', '06', '2017', 1, '2017-03-14 18:02:55', '2017-03-14 22:57:27'),
+(8, 'test 4', 'test description 4', 'test address 4', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-57-07_5255041a51a1ee4ec1633d2007f39595ef06b260.jpg', '07', '2017', 1, '2017-03-14 18:44:34', '2017-03-14 22:57:07'),
+(9, 'test', 'test desciption', 'test address', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-56-51_5075b05af4eccac0ed2724f6fe7722a768fba606.jpg', '07', '2017', 0, '2017-03-14 18:02:55', '2017-03-14 22:56:51'),
+(10, 'test 2`', 'test description 2', 'test address 2', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-56-37_38681ffdc30422ea1f7491c11a289ad0e16bee06.jpg', '07', '2017', 1, '2017-03-14 18:44:34', '2017-03-14 22:56:37'),
+(11, 'test3', 'test desciption3', 'test address3', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-56-23_f53c27ca0ca249e9d1799f300921b7a38183816f.jpg', '07', '2017', 1, '2017-03-14 18:02:55', '2017-03-14 22:56:23'),
+(12, 'test 4', 'test description 4', 'test address 4', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-15-12-02-51_7bceb24ed92a001eb33eabc26768328bbab3c7ab.jpg', '08', '2017', 0, '2017-03-14 18:44:34', '2017-03-14 23:02:51'),
+(13, 'test5', 'test desciption5', 'test address5', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-55-40_9ca99065f59cfe1cdae1e9b316c90e75aaa3c48b.jpg', '08', '2017', 1, '2017-03-14 18:02:55', '2017-03-14 22:55:40'),
+(14, 'test 6', 'test description 6', 'test address 6', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-55-11_2bb70922ab645166835bc497e8e25469d8323c06.jpg', '01', '2018', 0, '2017-03-14 18:44:34', '2017-03-14 22:55:11'),
+(15, 'test3', 'test desciption3', 'test address3', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-55-00_93085df1d86ea712757e561382c45be7dabb4901.jpg', '02', '2018', 1, '2017-03-14 18:02:55', '2017-03-14 22:55:00'),
+(16, 'test 4', 'test description 4', 'test address 4', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-54-46_dc1420e9b0d044dca593d43186e68f04ed59ef9b.jpg', '03', '2018', 1, '2017-03-14 18:44:34', '2017-03-14 22:54:46'),
+(17, 'test 6', 'test description 6', 'test address 6', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-54-22_54d5283fc6edf0678cacad7e074a6af7ea43c7b9.jpg', '04', '2018', 0, '2017-03-14 18:44:34', '2017-03-14 22:54:22'),
+(19, 'test 4', 'test description 4', 'test address 4', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-53-46_14396d974ba72e8f8be162ae3cb5b43a22bd6ca2.jpg', '04', '2018', 1, '2017-03-14 18:44:34', '2017-03-14 22:53:46'),
+(20, 'test 6', 'test description 6', 'test address 6', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-53-29_bb7fa0e572fb18477bb620702bc165f669c3bf0e.jpg', '06', '2018', 0, '2017-03-14 18:44:34', '2017-03-14 22:53:29'),
+(21, 'test3', 'test desciption3', 'test address3', '1', '2', '3', '4', 1, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-15-12-02-05_d1cd89f8c33bc6056e73d934ff060bc32a40bfde.jpg', '07', '2018', 1, '2017-03-14 18:02:55', '2017-03-14 23:02:05'),
+(22, 'test 4', 'test description 4', 'test address 4', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-14-11-52-42_f798c5b46ab407d1e842e37bba8d99165d46a5ad.jpg', '07', '2018', 1, '2017-03-14 18:44:34', '2017-03-14 22:52:42'),
+(23, 'test 6', 'test description 6', 'test address 6', '12', '22', '32', '42', 2, 0, 2, 1, 4, 1, 1, 1, 'layouts/images/bullding_image/17-03-15-12-04-51_38681ffdc30422ea1f7491c11a289ad0e16bee06.jpg', '08', '2018', 0, '2017-03-14 18:44:34', '2017-03-14 23:04:51');
 
 -- --------------------------------------------------------
 
@@ -103,50 +136,8 @@ CREATE TABLE `city` (
 --
 
 INSERT INTO `city` (`id`, `name`) VALUES
-(2, 'Cairo'),
+(2, 'Cairo2'),
 (1, 'Giza');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lands`
---
-
-CREATE TABLE `lands` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `square` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `describtion` text NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `city_id` int(10) NOT NULL,
-  `area_id` int(10) NOT NULL,
-  `subarea_id` int(10) NOT NULL,
-  `categoury_id` int(10) NOT NULL,
-  `subcategoury_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `shops`
---
-
-CREATE TABLE `shops` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `square` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `city_id` int(10) NOT NULL,
-  `area_id` int(10) NOT NULL,
-  `subarea_id` int(10) NOT NULL,
-  `categoury_id` int(10) NOT NULL,
-  `subcategoury_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -210,7 +201,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `phone`, `address`, `facebook`, `password`, `isadmin`) VALUES
-(1, 'Mohamed Zayed Mohamed', 'mohamedzayed709@yahoo.com', '01127946754', 'AinShams', 'https://www.facebook.com/', '601f1889667efaebb33b8c12572835da3f027f78', 1);
+(1, 'Mohamed Zayed Mohamed', 'mohamedzayed709@yahoo.com', '1096901954', 'AinShams', 'https://www.facebook.com/', '601f1889667efaebb33b8c12572835da3f027f78', 1);
 
 --
 -- Indexes for dumped tables
@@ -250,30 +241,6 @@ ALTER TABLE `city`
   ADD UNIQUE KEY `name` (`name`);
 
 --
--- Indexes for table `lands`
---
-ALTER TABLE `lands`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`,`city_id`,`area_id`,`subarea_id`,`categoury_id`,`subcategoury_id`),
-  ADD KEY `city_id` (`city_id`),
-  ADD KEY `area_id` (`area_id`),
-  ADD KEY `subarea_id` (`subarea_id`),
-  ADD KEY `categoury_id` (`categoury_id`),
-  ADD KEY `subcategoury_id` (`subcategoury_id`);
-
---
--- Indexes for table `shops`
---
-ALTER TABLE `shops`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`,`city_id`,`area_id`,`subarea_id`,`categoury_id`,`subcategoury_id`),
-  ADD KEY `city_id` (`city_id`),
-  ADD KEY `area_id` (`area_id`),
-  ADD KEY `subarea_id` (`subarea_id`),
-  ADD KEY `categoury_id` (`categoury_id`),
-  ADD KEY `subcategoury_id` (`subcategoury_id`);
-
---
 -- Indexes for table `sub_area`
 --
 ALTER TABLE `sub_area`
@@ -308,7 +275,7 @@ ALTER TABLE `area`
 -- AUTO_INCREMENT for table `buldings`
 --
 ALTER TABLE `buldings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `categouries`
 --
@@ -318,17 +285,7 @@ ALTER TABLE `categouries`
 -- AUTO_INCREMENT for table `city`
 --
 ALTER TABLE `city`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `lands`
---
-ALTER TABLE `lands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `shops`
---
-ALTER TABLE `shops`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `sub_area`
 --
@@ -338,12 +295,12 @@ ALTER TABLE `sub_area`
 -- AUTO_INCREMENT for table `sub_categouries`
 --
 ALTER TABLE `sub_categouries`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
@@ -364,28 +321,6 @@ ALTER TABLE `buldings`
   ADD CONSTRAINT `buldings_ibfk_4` FOREIGN KEY (`subarea_id`) REFERENCES `sub_area` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `buldings_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `buldings_ibfk_6` FOREIGN KEY (`subcategoury_id`) REFERENCES `sub_categouries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `lands`
---
-ALTER TABLE `lands`
-  ADD CONSTRAINT `lands_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lands_ibfk_2` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lands_ibfk_3` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lands_ibfk_4` FOREIGN KEY (`subarea_id`) REFERENCES `sub_area` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lands_ibfk_5` FOREIGN KEY (`categoury_id`) REFERENCES `categouries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lands_ibfk_6` FOREIGN KEY (`subcategoury_id`) REFERENCES `sub_categouries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `shops`
---
-ALTER TABLE `shops`
-  ADD CONSTRAINT `shops_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shops_ibfk_2` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shops_ibfk_3` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shops_ibfk_4` FOREIGN KEY (`subarea_id`) REFERENCES `sub_area` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shops_ibfk_5` FOREIGN KEY (`categoury_id`) REFERENCES `categouries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `shops_ibfk_6` FOREIGN KEY (`subcategoury_id`) REFERENCES `sub_categouries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `sub_area`
