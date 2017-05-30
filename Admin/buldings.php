f34728e69d8a848a1f1e252ef14a20c207e36ad2
alaaDragneel <mohamedzayed709@yahoo.com>
2017-05-30 04:37:48 +0200

    100% errors fixed

--- Admin/buldings.php
+++ Admin/buldings.php
@@ -822,13 +822,13 @@
                                            <label class="col-sm-2 control-label">survice name</label>
                                            <div class="col-sm-10">
                                                <input type="hidden" name="survice_check[]">
-                                               <input type="text" name="survice_name[]" class="form-control" placeholder="survice_name" required>
+                                               <input type="text" name="survice_name[]" class="form-control" placeholder="survice_name">
                                            </div>
                                        </div>
                                        <div class="form-group">
                                            <label class="col-sm-2 control-label">survice description</label>
                                            <div class="col-sm-10">
-                                               <textarea name="survice_description[]" maxlength="250" class="form-control textarea" placeholder="description" rows="4" cols="80" required></textarea>
+                                               <textarea name="survice_description[]" maxlength="250" class="form-control textarea" placeholder="description" rows="4" cols="80"></textarea>
                                                <div></div>
                                            </div>
                                        </div>
